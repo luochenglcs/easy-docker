febbd8465387203ad6ba492864c134c49901ff99
luffyluo <luffyluo@tencent.com>
2024-01-09 11:53:08 +0800

    update

--- Docker-impl/got_root.sh
+++ Docker-impl/got_root.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+Tag=${1:-'ubuntu'}
 ls /bin/docker >/dev/null || yum install docker
-docker pull $1
+docker pull $Tag
+rm -rf root
 mkdir root
 cd root
-docker export -o $1.tar `docker run --name test -d $1 top -b`
+docker export -o $Tag.tar `docker run --name test -d $Tag top -b`
 docker stop test
 docker remove test
-tar -axvf $1.tar
-rm -rf $1.tar
+tar -axvf $Tag.tar
+rm -rf $Tag.tar
 cd ..
